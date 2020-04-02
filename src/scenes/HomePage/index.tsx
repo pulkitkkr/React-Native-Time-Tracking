b1e3ef492b722a42197901a2df3d52cc35ef1253
pulkitkkr <pulkitkkr@gmail.com>
2020-04-02 23:54:33 +0530

    Update Folder Structure

--- src/scenes/HomePage/index.tsx
+++ src/scenes/HomePage/index.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { SafeAreaView, ScrollView, Text, StatusBar } from 'react-native';
 import Env from 'react-native-config';
-import css from './HomeScreen.scss';
+import css from './index.scss';
 
-const HomeScreen = () => (
+const Index = () => (
   <>
     <StatusBar barStyle="dark-content" />
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text className={css.someClass}>{Env.API_HOST}</Text>
+        <Text className={css.someClass}>{Env.API_HOST} Hai</Text>
       </ScrollView>
     </SafeAreaView>
   </>
 );
 
-export default HomeScreen;
+export default Index;

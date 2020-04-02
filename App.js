2c7d293afd33486b748c345a3a8b96e84548dc1f
pulkitkkr <pulkitkkr@gmail.com>
2020-04-02 18:11:32 +0530

    Add Podfile related things & Env support

---
+++ App.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __importStar(require("react"));
+var HomeScreen_1 = __importDefault(require("./src/HomeScreen"));
+var App = function () {
+    return (<>
+      <HomeScreen_1.default />
+    </>);
+};
+exports.default = App;
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { SafeAreaView, ScrollView, Text, StatusBar } from 'react-native';
+import Env from 'react-native-config';
 import css from './HomeScreen.scss';
 
 const HomeScreen = () => (
@@ -7,7 +8,7 @@ const HomeScreen = () => (
     <StatusBar barStyle="dark-content" />
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text className={css.someClass}>Hello</Text>
+        <Text className={css.someClass}>{Env.API_HOST}</Text>
       </ScrollView>
     </SafeAreaView>
   </>

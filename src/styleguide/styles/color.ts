8d3d08f05969fd253e030f43a46498602340f8fb
pulkitkkr <pulkitkkr@gmail.com>
2020-04-23 20:23:45 +0530

    Update Color Palette

--- src/styleguide/styles/color.ts
+++ src/styleguide/styles/color.ts
@@ -1,10 +1,25 @@
-export type UiColor = 'purple' | 'white' | 'lightPurple' | 'transparent' | 'black';
+export type UiColor =
+  | 'darkerPurple'
+  | 'darkPurple'
+  | 'purple'
+  | 'white'
+  | 'lightPurple'
+  | 'lighterPurple'
+  | 'red'
+  | 'lighterRed'
+  | 'transparent'
+  | 'black';
 
 const colorsMapping: { [k in UiColor]: string } = {
-  purple: '#5628EE',
+  darkerPurple: '#360AC3',
+  darkPurple: '#5728EE',
+  purple: '#8562F6',
   lightPurple: '#8c6ff0',
+  lighterPurple: '#ccc9e6',
   white: '#fff',
-  black: '#000',
+  black: '#000314',
+  red: '#aa1f48',
+  lighterRed: '#e6d3d4',
   transparent: '#00000000',
 };
 

4269339f1fd0b57ba20421170d344a0ab1fc0bee
pulkitkkr <pulkitkkr@gmail.com>
2020-05-03 22:25:20 +0530

    Rename Project to PomoDoro

--- android/app/src/main/java/com/pomodoro/MainActivity.java
+++ android/app/src/main/java/com/pomodoro/MainActivity.java
@@ -1,4 +1,4 @@
-package com.followgram;
+package com.pomodoro;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FollowGram";
+    return "PomoDoro";
   }
 }
